f9e49ceb54481b6bd387d2c948fbb8e359a988b2
Eric Pak <epak@zendesk.com>
2019-05-30 14:41:00 +1000

    Add logging helpers

---
+++ pakkr/logging.py
@@ -0,0 +1,21 @@
+import logging
+import time
+
+from contextlib import contextmanager
+
+
+class IndentationAdapter(logging.LoggerAdapter):
+    def process(self, msg, kwargs):
+            return '{indent}{identifier} - {msg}'.format(indent=' '*(4*self.extra['indent']),
+                                                         identifier=self.extra['identifier'],
+                                                         msg=msg),\
+                   kwargs
+
+
+@contextmanager
+def log_timing(logger):
+    logger.info("starting")
+    start_time = time.time()
+    yield
+    end_time = time.time()
+    logger.info("finished (took {elapsed:.3f}s)".format(elapsed=end_time - start_time))
---
+++ pakkr/logging_test.py
@@ -0,0 +1,22 @@
+from mock import MagicMock, patch, call
+from pakkr.logging import IndentationAdapter, log_timing
+
+
+def test_indentAdapter():
+    adapter = IndentationAdapter(None, {'indent': 1,
+                                        'identifier': "some_obj"})
+
+    kwargs = {'x': 1}
+    assert adapter.process("hello", kwargs) == ("    some_obj - hello", kwargs)
+
+
+@patch('pakkr.logging.time')
+def test_log_timing(mock_time):
+    mock_time.time.side_effect = [1.0, 9.0]
+    mock_logger = MagicMock()
+    mock_step = MagicMock()
+    with log_timing(mock_logger):
+        mock_step.run(x=1)
+
+    mock_step.run.assert_called_with(x=1)
+    mock_logger.info.assert_has_calls([call("starting"), call("finished (took 8.000s)")])

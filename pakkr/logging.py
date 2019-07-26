6b3aa35bce7cafb4b07680e81dbb5a4a44eedc48
Eric Pak <epak@zendesk.com>
2019-07-26 15:45:01 +1000

    Allow suppressing timing logs

--- pakkr/logging.py
+++ pakkr/logging.py
@@ -13,9 +13,12 @@ class IndentationAdapter(logging.LoggerAdapter):
 
 
 @contextmanager
-def log_timing(logger):
-    logger.info("starting")
-    start_time = time.time()
-    yield
-    end_time = time.time()
-    logger.info("finished (took {elapsed:.3f}s)".format(elapsed=end_time - start_time))
+def log_timing(logger, suppressd=False):
+    if suppressd:
+        yield
+    else:
+        logger.info("starting")
+        start_time = time.time()
+        yield
+        end_time = time.time()
+        logger.info("finished (took {elapsed:.3f}s)".format(elapsed=end_time - start_time))
